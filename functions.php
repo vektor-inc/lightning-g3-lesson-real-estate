19768bf3a04947f8b342369892e2269e88214d15
kurudrive <kurudrive@gmail.com>
2021-05-26 03:55:55 +0900

    カスタム投稿タイプ、カスタム分類の表示まで

--- functions.php
+++ functions.php
@@ -50,3 +50,42 @@ function my_add_custom_taxonomy() {
 
 }
 add_action( 'init', 'my_add_custom_taxonomy', 0 );
+
+function my_custom_loop() {
+
+	// 表示条件を発行
+	$args         = array( 
+		'post_type' => 'chintai', /* 表示する投稿タイプ */
+		'posts_per_page' => 12,
+	);
+	$custom_query     = new WP_Query( $args );
+
+	// 表示するHT<L
+	$options = array(
+		// card, card-noborder, card-intext, card-horizontal , media, postListText
+		'layout'                     => 'card-noborder',
+		'display_image'              => true,
+		'display_image_overlay_term' => true,
+		'display_excerpt'            => true,
+		'display_date'               => false,
+		'display_new'                => true,
+		'display_taxonomies'         => true,
+		'display_btn'                => false,
+		'image_default_url'          => false,
+		'overlay'                    => false,
+		'btn_text'                   => __( 'Read more', 'lightning' ),
+		'btn_align'                  => 'text-right',
+		'new_text'                   => __( 'New!!', 'lightning' ),
+		'new_date'                   => 7,
+		'class_outer'                => 'vk_post-col-sm-6 vk_post-col-md-4 vk_post-col-xl-3',
+		'class_title'                => '',
+		'body_prepend'               => '',
+		'body_append'                => '',
+	);
+	$options_loop = array(
+		'class_loop_outer' => '',
+	);
+	$loop_html = VK_Component_Posts::get_loop( $custom_query, $options, $options_loop );
+	return $loop_html;
+}
+add_shortcode( 'my_custom_loop', 'my_custom_loop' );
